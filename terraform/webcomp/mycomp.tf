6a4897de37e08a0aa9b85ab9c22c220f2314c17c
solomonyaa <solomonyaa@gmail.com>
2026-04-14 21:26:25 +0300

    Add Terraform config

---
+++ terraform/webcomp/mycomp.tf
@@ -0,0 +1,55 @@
+resource "aws_security_group" "my_security_group" {
+  name   = "my_security_group"
+  vpc_id = var.vpc_id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 5000
+  ip_protocol       = "tcp"
+  to_port           = 5000
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_instance" "ec2-1" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  key_name               = "solomon's-key-pair"
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum install -y docker
+    systemctl start docker
+    systemctl enable docker
+    docker pull solomony/scheduler-api:latest
+    docker run -d -p 5000:5000 solomony/scheduler-api:latest
+  EOF
+
+  tags = { Name = "CI-CD server" }
+}
